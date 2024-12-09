4b2e41af23491e8fea8360077e7e2e8a3855186c
richard.mabry <rich@themabryfamily.com>
2024-12-09 13:31:28 -0700

    Day 10 prep

---
+++ Day10.cs
@@ -0,0 +1,10 @@
+﻿//https://adventofcode.com/2024/day/10
+namespace AdventOfCode
+{
+    class Day10
+    {
+        public void Run()
+        {
+        }
+    }
+}
--- Day9.cs
+++ Day9.cs
@@ -1,4 +1,4 @@
-﻿//https://adventofcode.com/2024/day/8
+﻿//https://adventofcode.com/2024/day/9
 namespace AdventOfCode
 {
     class Day9
