55e7d5cbec6b2332fdb6fcf23c5e9902d1fbb3b1
richard.mabry <rich@themabryfamily.com>
2024-12-13 19:22:27 -0700

    Day 14 prep

---
+++ Day14.cs
@@ -0,0 +1,11 @@
+﻿//https://adventofcode.com/2024/day/14
+
+namespace AdventOfCode
+{
+    class Day14
+    {
+        public void Run()
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            var day = new Day13();
+            var day = new Day14();
             day.Run();
         }
     }
