a016acd0f41fbd321beeaa8ea3f6a2dbe916eebb
richard.mabry <rich@themabryfamily.com>
2024-12-22 11:05:19 -0700

    Final prep

---
+++ Day24.cs
@@ -0,0 +1,14 @@
+﻿//https://adventofcode.com/2024/day/24
+namespace AdventOfCode
+{
+    class Day24
+    {
+        public Day24()
+        {
+        }
+
+        public void Run()
+        {
+        }
+    }
+}
---
+++ Day25.cs
@@ -0,0 +1,14 @@
+﻿//https://adventofcode.com/2024/day/25
+namespace AdventOfCode
+{
+    class Day25
+    {
+        public Day25()
+        {
+        }
+
+        public void Run()
+        {
+        }
+    }
+}
