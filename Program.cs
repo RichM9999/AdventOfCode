9fc591aadfba93897fcbc6f25ab077decb8041e3
Richard Mabry <richard.mabry@filmtrack.net>
2025-12-03 11:23:46 -0700

    Run multiple days

--- Program.cs
+++ Program.cs
@@ -4,8 +4,20 @@
     {
         static void Main()
         {
-            var day = new Year2025.Day3();
-            day.Run();
-       }
+            Console.WriteLine("Day1:");
+            var day1 = new Year2025.Day1();
+            day1.Run();
+            Console.WriteLine();
+
+            Console.WriteLine("Day2:");
+            var day2 = new Year2025.Day2();
+            day2.Run();
+            Console.WriteLine();
+
+            Console.WriteLine("Day3:");
+            var day3 = new Year2025.Day3();
+            day3.Run();
+            Console.WriteLine();
+        }
     }
 }
